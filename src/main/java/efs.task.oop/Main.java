fa012933531c1c1da0d427655050221a6be24c45
Kamil <82053152+Kasy00@users.noreply.github.com>
2023-05-21 14:29:28 +0200

    Update Main.java

--- src/main/java/efs.task.oop/Main.java
+++ src/main/java/efs.task.oop/Main.java
@@ -1,5 +1,6 @@
 package efs.task.oop;
 
+
 public class Main {
     public static void main(String[] args) {
         Villager kashya = new Villager("Kashya", 30);
@@ -19,32 +20,32 @@ public class Main {
         warriv.sayHello();
         flawia.sayHello();
         while (Monsters.monstersHealth > 0) {
-            if (kashya.getIsDead() != true) {
+            if (kashya.getHealth() > 0) {
                 kashya.attack(Monsters.andariel);
                 Monsters.andariel.attack(kashya);
             }
 
-            if (akara.getIsDead() != true) {
+            if (akara.getHealth() > 0) {
                 akara.attack(Monsters.blacksmith);
                 Monsters.blacksmith.attack(akara);
             }
 
-            if (gheed.getIsDead() != true) {
+            if (gheed.getHealth() > 0) {
                 gheed.attack(Monsters.andariel);
                 Monsters.andariel.attack(gheed);
             }
 
-            if (deckardCain.getIsDead() != true) {
+            if (deckardCain.getHealth() > 0) {
                 deckardCain.attack(Monsters.blacksmith);
                 Monsters.blacksmith.attack(deckardCain);
             }
 
-            if (warriv.getIsDead() != true) {
+            if (warriv.getHealth() > 0) {
                 warriv.attack(Monsters.andariel);
                 Monsters.andariel.attack(warriv);
             }
 
-            if (flawia.getIsDead() != true) {
+            if (flawia.getHealth() > 0) {
                 flawia.attack(Monsters.blacksmith);
                 Monsters.blacksmith.attack(flawia);
             }
