2a386ae3146df3b18a5962892c38aa803a9d0222
Kamil <82053152+Kasy00@users.noreply.github.com>
2023-05-21 13:57:22 +0200

    Update Main.java

--- src/main/java/efs.task.oop/Main.java
+++ src/main/java/efs.task.oop/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Villager kashya = new Villager("Kashya", 30);
         ExtraordinaryVillager akara = new ExtraordinaryVillager("Akara", 40, ExtraordinaryVillager.Skill.SHELTER);
         Villager gheed = new Villager("Gheed", 50);
-        ExtraordinaryVillager deckardCain = new ExtraordinaryVillager("Decard Cain", 85, ExtraordinaryVillager.Skill.IDENTIFY);
+        ExtraordinaryVillager deckardCain = new ExtraordinaryVillager("Deckard Cain", 85, ExtraordinaryVillager.Skill.IDENTIFY);
         Villager warriv = new Villager("Warriv", 35);
         Villager flawia = new Villager("Flawia", 25);
 
